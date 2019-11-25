3fef8d84cc3a52df362be21e42c909ef590fe0c2
melonfruity <eric.luo24@gmail.com>
2019-11-25 16:02:26 -0500

    added eqArrays function

---
+++ tail.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const tail = (arr) => arr.slice(1);
+
+// TEST CODE
+const assertEqual = function(actual, expected) {
+  const message = actual === expected ? `Assertion Passed: ${actual} === ${expected}` : `Assertion Failed: ${actual} !== ${expected}`;
+  console.log(message);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
